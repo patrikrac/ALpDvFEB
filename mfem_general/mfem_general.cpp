8315be135d751e2137436c6534497db528f963d2
Patrik Rác <patri_rac@gmx.de>
2021-10-12 17:43:23 +0200

    Added Netgen/NGSolve file. (Broken) but correct MAkefile and mfem source

---
+++ mfem_general/mfem_general.cpp
@@ -0,0 +1,347 @@
+//Created by Patrik Rác 
+//This is a general MFEM program designed to solve Laplace-equations using the MFEM Library.
+//The dimensionality of the programm can be changed by altering the input grid.
+
+#include "mfem.hpp"
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+using namespace mfem;
+
+// Boundary and right-hand side functions that determine the general behaviour of the programm.
+double bdr_func(const Vector &p);
+double rhs_func(const Vector &p);
+
+
+//Metrics to be collected for later graphing.
+typedef struct error_values {
+   int cycle; 
+   int cells;
+   int dofs;
+   double error;
+   double relative_error;
+} error_values;
+
+
+//----------------------------------------------------------------
+//Class for the specification of the problem
+//----------------------------------------------------------------
+class Problem
+{
+   public:
+      Problem(): hysteresis(0.0), max_elem_error(1.0e-10), order(2)
+      {
+      }
+      void run();
+
+   private:
+      void make_mesh();
+
+      void update(BilinearForm &a, LinearForm &f, FiniteElementSpace &fespace, GridFunction &x, GridFunction &error_zero);
+      void solve(BilinearForm &a, LinearForm &f, FiniteElementSpace &fespace, GridFunction &x, Array<int> &ess_bdr, FunctionCoefficient &bdr);
+      bool refine(BilinearForm &a, LinearForm &f, FiniteElementSpace &fespace, GridFunction &x, GridFunction &error_zero, ThresholdRefiner &refiner);
+
+      void exact_error(int cycle, int dofs, GridFunction &x, GridFunction &error_zero, FunctionCoefficient &u);
+
+      void output_table();
+      void glvis_output(GridFunction &x);
+      void vtk_output(GridFunction &x);
+      
+      //Configuration parameters
+      double hysteresis;   //derefinement safety coefficient
+      double max_elem_error;
+      int order;
+
+      //Data parameters
+      Mesh mesh;
+      vector<error_values> table_vector;      
+};
+
+
+//----------------------------------------------------------------
+//Create the mesh the problem will be solved on
+//----------------------------------------------------------------
+void Problem::make_mesh()
+{
+  const char *mesh_file = "unit_cube.mesh";
+  mesh = Mesh::LoadFromFile(mesh_file);
+  for (int i = 0; i < 2; i ++) 
+   {
+       mesh.UniformRefinement();
+   }
+   
+   //Turns the Quad mesh (which supports hanging nodes) to a tetrahedral mesh without hanging nodes
+   mesh = Mesh::MakeSimplicial(mesh);
+
+   mesh.Finalize(true);
+
+   cout << "Mesh generated." << endl;
+}
+
+
+//----------------------------------------------------------------
+//Update all variables to adabt to the recently adapted mesh
+//----------------------------------------------------------------
+void Problem::update(BilinearForm &a, LinearForm &f, FiniteElementSpace &fespace, GridFunction &x, GridFunction &error_zero)
+{
+   fespace.Update();
+
+   x.Update();
+
+   error_zero.Update();
+   error_zero = 0.0;
+
+   fespace.UpdatesFinished();
+
+   // Inform the linear and bilinear forms that the space has changed.
+   a.Update();
+   f.Update();
+}
+
+
+//----------------------------------------------------------------
+//Solve the Problem on the current mesh
+//----------------------------------------------------------------
+void Problem::solve(BilinearForm &a, LinearForm &f, FiniteElementSpace &fespace, GridFunction &x, Array<int> &ess_bdr, FunctionCoefficient &bdr)
+{
+   a.Assemble();
+   f.Assemble();
+
+   // Project the exact solution to the essential boundary DOFs.
+   x.ProjectBdrCoefficient(bdr, ess_bdr);
+
+   //Create and solve the linear system.
+   Array<int> ess_tdof_list;
+   fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+
+   SparseMatrix A;
+   Vector B, X;
+
+   a.FormLinearSystem(ess_tdof_list, x, f, A, X, B);
+
+   GSSmoother M(A);
+   PCG(A, M, B, X, 0, 500, 1e-12, 0.0);
+
+   a.RecoverFEMSolution(X, f, x);
+
+}
+
+
+//----------------------------------------------------------------
+//Execute one refinement step and call the update funciton to adapt the other variables
+//----------------------------------------------------------------
+bool Problem::refine(BilinearForm &a, LinearForm &f, FiniteElementSpace &fespace, GridFunction &x, GridFunction &error_zero, ThresholdRefiner &refiner)
+{
+   refiner.Apply(mesh);
+   
+   if (refiner.Stop())
+   {
+      return false;
+   }
+
+   // 20. Update the space and interpolate the solution.
+   update(a, f, fespace, x, error_zero);
+
+   return true;
+}
+
+
+//----------------------------------------------------------------
+//This method initializes a lot of the functionality of the programm
+//Run the problem and terminate after a given condition
+//----------------------------------------------------------------
+void Problem::run()
+{
+   //Mesh Generation
+   make_mesh();
+
+   //Declaration
+   int dim = mesh.Dimension();
+   int sdim = mesh.SpaceDimension();
+
+   //Setup the necessary spaces and solutions for the problem
+   H1_FECollection fec(order, dim);
+   FiniteElementSpace fespace(&mesh, &fec);
+
+   BilinearForm a(&fespace);
+   LinearForm f(&fespace);
+
+   GridFunction x(&fespace);
+
+   //Grid function for calculation of the relative norm
+   GridFunction error_zero(&fespace);
+   error_zero = 0.0;
+
+   FunctionCoefficient u(bdr_func);
+
+   Array<int> ess_bdr(mesh.bdr_attributes.Max());
+   ess_bdr = 1;
+
+   
+   //Setup
+   ConstantCoefficient one(1.0);
+   FunctionCoefficient rhs(rhs_func);
+   FunctionCoefficient bdr(bdr_func);
+
+
+   //Specify the Problem
+   BilinearFormIntegrator *integ = new DiffusionIntegrator(one);
+   a.AddDomainIntegrator(integ);
+   f.AddDomainIntegrator(new DomainLFIntegrator(rhs));
+
+
+   KellyErrorEstimator *estimator{nullptr};
+   L2_FECollection flux_fec(order, dim);
+
+   auto flux_fes = new FiniteElementSpace(&mesh, &flux_fec, sdim);
+   estimator = new KellyErrorEstimator(*integ, x, flux_fes);
+
+
+   ThresholdRefiner refiner(*estimator);
+   refiner.SetTotalErrorFraction(0.0); // use purely local threshold
+   refiner.SetLocalErrorGoal(max_elem_error);
+   refiner.PreferConformingRefinement();
+   refiner.SetNCLimit(0);
+
+   ThresholdDerefiner derefiner(*estimator);
+   derefiner.SetThreshold(hysteresis * max_elem_error);
+   derefiner.SetNCLimit(0);
+   
+   x=0.0;
+
+   refiner.Reset();
+   derefiner.Reset();
+
+   int step = 0;
+   while (true)
+   {
+
+      cout << "Step: " << step << endl << "DOF: " << fespace.GetNDofs()<< endl;  
+
+      solve(a,f,fespace,x,ess_bdr, bdr);
+
+      exact_error(step,fespace.GetNDofs(), x, error_zero, u);
+
+      //Stop the loop if no more elements are marked for refinement or the desired number of DOFs is reached.
+      if(!refine(a, f, fespace, x, error_zero ,refiner) || fespace.GetNDofs() > 20000)
+      {
+         break;
+      }
+      step++;
+   }
+
+
+   if (derefiner.Apply(mesh))
+   {
+      cout << "\nDerefined elements." << endl;
+
+      update(a,f,fespace,x, error_zero);
+   }
+
+   cout << "Final: " << step << endl << "DOF: " << fespace.GetNDofs()<< endl;  
+
+   a.Update();
+   f.Update();
+   
+   delete estimator;
+
+   vtk_output(x);
+   output_table();
+}
+
+
+//----------------------------------------------------------------
+//Calculate the exact error 
+//----------------------------------------------------------------
+void Problem::exact_error(int cycle, int dofs, GridFunction &x, GridFunction &error_zero, FunctionCoefficient &u)
+{
+   error_values values = {};
+   values.cycle = cycle;
+   values.dofs = dofs;
+   values.cells = mesh.GetNE();
+   values.error = x.ComputeMaxError(u);
+   values.relative_error = values.error / error_zero.ComputeMaxError(u);
+
+   table_vector.push_back(values);
+
+   cout << "Error for step " << cycle << ": " << setprecision(3) << scientific << values.error << endl;
+}
+
+
+//----------------------------------------------------------------
+//Output the table containing the calcualted errors
+//----------------------------------------------------------------
+void Problem::output_table()
+{
+   std::ofstream output("table.tex");
+   std::ofstream output_custom("errorMFEM.txt");
+
+   output_custom << "MFEM" << endl;
+   output_custom << "$n_\\text{dof}$" << endl;
+   output_custom << "$\\norm{u - u_h}_{L^\\infty}$" << endl;
+   output_custom << table_vector.size() << endl;
+
+   output << "\\begin{table}[h]" << endl;
+   output << "\t\\begin{center}" << endl;
+   output << "\t\t\\begin{tabular}{|c|c|c|c|c|} \\hline" << endl;
+   
+   output << "\t\t\tcycle & \\# cells & \\# dofs & $\\norm{u - u_h}_{L^\\infty}$ & $\\dfrac{\\norm{u - u_h}_{L^\\infty}}{\\norm{u}_{L^\\infty}}$\\\\ \\hline" << endl;
+   for (int i = 0; i < table_vector.size(); i++)
+   {
+      output << "\t\t\t" << table_vector[i].cycle << " & " << table_vector[i].cells << " & " << table_vector[i].dofs << " & " << setprecision(3) << scientific << table_vector[i].error << " & " << setprecision(3) << scientific << table_vector[i].relative_error << "\\\\ \\hline" << endl;
+      output_custom << table_vector[i].dofs << " " << table_vector[i].error << endl;
+   }
+
+   output << "\t\t\\end{tabular}" <<endl;
+   output << "\t\\end{center}" << endl;
+   output << "\\end{table}" << endl;
+}
+
+
+//----------------------------------------------------------------
+//Create a vtk Output for the current solution
+//----------------------------------------------------------------
+void Problem::vtk_output(GridFunction &x)
+{
+   std::ofstream output("solution.vtk");
+
+   mesh.PrintVTK(output, 0);
+   x.SaveVTK(output, "u", 0);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+   Problem l;
+   l.run();
+}
+
+
+// Exact solution, used for the Dirichlet BC.
+double bdr_func(const Vector &p)
+{
+   double radius = sqrt(p(0)*p(0) + p(1)*p(1));
+   double phi;
+   double alpha = 1.0/2.0;
+
+   if(p(1) < 0)
+   {
+      phi = 2*M_PI+atan2(p(1),p(0));
+   }
+   else
+   {
+      phi = atan2(p(1),p(0));
+   }
+
+   return pow(radius,alpha) * sin(alpha * phi);
+}
+
+// Right hand side function
+double rhs_func(const Vector &p)
+{
+   return -200*exp(-10*(p(0)+p(1)));
+}
---
+++ ngsolve_general/ngsolve-general.py
@@ -0,0 +1,233 @@
+"""
+Created by Patrik Rác
+Solution of the following problem
+-Solving the Laplace on a defined mesh. 
+The problem is solved using an adaptivity strategy with an gradient recovery error estimator.
+The adaptivity and Problem geometry can be easily changed without affecting the performance of the programm.
+The programm runns with the command "netgen ngsolve-general.py or python3.8 ngsolve-general.py"
+"""
+from ngsolve import *
+from netgen.csg import *
+
+class Problem:
+
+    def __init__(self):
+        
+        #Define the general parameters and functions for the Problem
+        #Define the parameter alpha which is dependant on the used geometry
+        self.alpha = 1.0/2.0
+        self.r = sqrt(x*x + y*y)
+        self.phi = atan2(y,x)
+
+        #Define the boundary function g
+        #self.g = CoefficientFunction([(self.r**self.alpha)*sin(self.alpha*self.phi) if bc=="L" else (self.r**self.alpha)*sin(self.alpha*(2*math.pi + self.phi)) if bc=="I" else 0 for bc in self.mesh.GetBoundaries()])
+        self.g = (self.r**self.alpha)*sin(self.alpha*self.phi)*(z**2)
+
+        #The exact solution of the problem. The mesh is divided into different materiels through a line. This is necessary in order to define teh function but can be ommited if the errror estimation isn't wanted.
+        #self.uexact = CoefficientFunction([(self.r**self.alpha)*sin(self.alpha*self.phi) if m=="upper" else (self.r**self.alpha)*sin(self.alpha*(2*math.pi + self.phi)) if m=="lower" else 0 for m in self.mesh.GetMaterials()])
+        self.uexact = (self.r**self.alpha)*sin(self.alpha*self.phi)*(z**2)
+
+        #Generate the mesh
+        self.mesh = self.make_mesh()
+
+        #Table list used for storing the error in each iteration
+        self.table_list = []
+        
+        #Setup the FE-Space and the Solution vector with proper boundary conditions, as well as the space and solution for the error estimation
+        (self.fes, self.gfu, self.space_flux, self.gf_flux) = self.setup_space()
+
+        #Get the Bilinear and Linear form aswell as the solver.
+        (self.a, self.f, self.c) = self.setup_system()
+
+
+    def make_mesh(self):
+        """
+        Initialize the problem mesh.
+        Dimensionality has to be taken into account.
+        """
+
+        brick = OrthoBrick(Pnt(-0.5,0,0), Pnt(0.5,1,1)).bc('bnd')
+
+        geo = CSGeometry()
+        geo.Add (brick)
+
+        return Mesh(geo.GenerateMesh(maxh=0.25))
+        
+
+    def setup_space(self):
+        """
+        Initialize the problem space and the space for the error estimator.
+        Order of the used FE-Elements can be changed here.
+        """
+        fes = H1(self.mesh, order=2, dirichlet="bnd", autoupdate=True)
+
+        #Set up the Solution vector on the Finite Element Space and set the boundary conditions
+        gfu = GridFunction(fes, autoupdate=True)
+        gfu.Set(self.g, definedon=self.mesh.Boundaries("bnd"))
+        
+        #Setup the space and solution for the error estimation using a ZZ estimator
+        space_flux = HDiv(self.mesh, order=2, autoupdate=True)
+        gf_flux = GridFunction(space_flux, autoupdate=True)
+
+        return (fes, gfu, space_flux, gf_flux)
+
+
+    def setup_system(self):
+        """
+        Define the Problem itself using its weak formulation and therefore the Biliniar and Linear form.
+        Initialize the Preconditioner for the solution.
+        The Bilinear and Linear Form of the Programm can be changed here. (Right hand side of the equation)
+        """
+        #Define Test and Trial functions
+        u, v = self.fes.TnT()
+
+        #Define the Bilinear form corresponding to the given problem
+        a = BilinearForm(self.fes)
+        a += grad(u)*grad(v)*dx
+
+        #Define the Linear form corresponding to the given problem
+        f = LinearForm(self.fes)
+        f += 2*v*dx
+
+        #Define the solver to be used to solve the problem
+        c = Preconditioner(a, "local")
+
+        return (a,f,c)
+    
+    
+    def calculate_error(self):
+        """
+        Estimate the error using an ZZ-error estimator described in the documentation.
+        """
+        # FEM-flux
+        flux =  grad(self.gfu)
+
+        # interpolate into H(div)
+        self.gf_flux.Set(flux)
+
+        # compute estimator:
+        err = (flux-self.gf_flux)*(flux-self.gf_flux)
+        eta2 = Integrate(err, self.mesh, VOL, element_wise=True)
+
+        # mark for refinement:
+        maxerr = max(eta2)
+        print ("maxerr: ", maxerr)
+
+        for el in self.mesh.Elements():
+            self.mesh.SetRefinementFlag(el, eta2[el.nr] > 0.25*maxerr)
+
+
+    def solve(self):
+        """
+        Solve the Problem by assembling the system and using the predefined solver.
+        """
+        self.a.Assemble()
+
+        self.f.Assemble()
+
+        self.c.Update()
+
+        solvers.BVP(bf=self.a, lf=self.f, gf=self.gfu, pre=self.c)
+
+
+    def output(self):
+        """
+        Create an vtk output file.
+        """
+        # VTKOutput object
+        vtk = VTKOutput(ma=self.mesh,
+                        coefs=[self.gfu,],
+                        names = ["u",],
+                        filename="result",
+                        subdivision=0)
+
+
+        # Exporting the results:
+        vtk.Do()
+
+
+    def exact_error(self, cycle):
+        """
+        Takes the current solution and the defined real solution and calculates the exact error.
+        The function stores the absolute and relative along with other metrics in the table_list array.
+        """
+        values = []
+        values.append(cycle)
+
+        values.append(len([el for el in self.mesh.Elements()]))
+
+        values.append(self.fes.ndof)
+
+        l2_error = sqrt(Integrate((self.gfu - self.uexact)*(self.gfu - self.uexact), self.mesh))
+        #values.append(l2_error)
+
+        max_error = max(Integrate((self.gfu - self.uexact), self.mesh, VOL, element_wise=True))
+        print("Max-error: ",  max_error)
+        values.append(max_error)
+
+        l2_relative_error = sqrt(Integrate((self.gfu - self.uexact)*(self.gfu - self.uexact),self.mesh)) / sqrt(Integrate(self.uexact*self.uexact, self.mesh))
+        #values.append(l2_relative_error)
+        #print("Relative error: ", l2_relative_error)
+
+        max_relative_error = max_error / max(Integrate(self.uexact,  self.mesh, VOL, element_wise=True))
+        values.append(max_relative_error)
+
+        self.table_list.append(values)
+
+
+    def output_Table(self):
+        """
+        Uses the table list vector to output an .tex file containing a table with the collected data.
+        """
+        f =open("table.tex", 'w')
+        f.write("\\begin{table}[h]\n")
+        f.write("\t\\begin{center}\n")
+        f.write("\t\t\\begin{tabular}{|c|c|c|c|c|} \hline\n")
+
+        plot_f = open("errorNG.txt", 'w')
+        plot_f.write("$NGSolve$\n")
+        plot_f.write("$n_\\text{dof}$\n")
+        plot_f.write("$\\norm{u - u_h}_{L^\\infty}$\n")
+        plot_f.write("{}\n".format(len(self.table_list)))
+
+        f.write("\t\t\tcycle & \# cells & \# dofs & $\\norm{u - u_h}_{L^\\infty}$ & $\dfrac{\\norm{u - u_h}_{L^\\infty}}{\\norm{u}_{L^\\infty}}$\\\ \hline\n")
+        for cycle, cells, dofs, error, relative_error in self.table_list:
+            f.write("\t\t\t{} & {} & {} & {:.3e} & {:.3e}\\\ \hline\n".format(cycle, cells, dofs, error, relative_error))
+            plot_f.write("{} {}\n".format(dofs, error))
+
+
+        f.write("\t\t\end{tabular}\n")
+        f.write("\t\end{center}\n")
+        f.write("\\end{table}")
+        f.close()
+        plot_f.close()
+
+
+    def do(self):
+        """
+        Execute the computation of the solution. Solve the problem using an adaptive mesh until a certain number of degrees of freedom are reached.
+
+        """
+        cycle = 0
+        while self.fes.ndof < 20000:
+            self.mesh.Refine()
+
+            self.gfu.Set(self.g, definedon=self.mesh.Boundaries("bnd"))
+
+            self.solve()
+
+            self.exact_error(cycle)
+
+            self.calculate_error()
+
+            print(self.fes.ndof)
+            cycle += 1
+            
+        self.output()
+        self.output_Table()
+        
+
+
+if __name__ == "__main__":
+    e = Problem()
+    e.do()
