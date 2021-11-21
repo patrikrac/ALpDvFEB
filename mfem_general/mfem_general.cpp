80bb8cc42250a445f548bed63533f9d5853d3924
Patrik Rác <patri_rac@gmx.de>
2021-11-21 15:32:15 +0100

    changed includes to work w cmake compilation

--- mfem_general/mfem_general.cpp
+++ mfem_general/mfem_general.cpp
@@ -2,13 +2,14 @@
 //This is a general MFEM program designed to solve Laplace-equations using the MFEM Library.
 //The dimensionality of the programm can be changed by altering the input grid.
 
+#include "mfem.hpp"
 
 #include <vector>
 #include <fstream>
 #include <iostream>
 #include <math.h>
 
-#include <mfem.hpp>
+
 
 using namespace std;
 using namespace mfem;
