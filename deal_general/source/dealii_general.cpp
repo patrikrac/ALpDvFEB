766db4c5f8d48b53fa2e6ba4f1bb77482ab80180
Patrik Rác <patri_rac@gmx.de>
2021-12-13 16:16:33 +0100

    changed include in general file

--- deal_general/source/dealii_general.cpp
+++ deal_general/source/dealii_general.cpp
@@ -3,8 +3,8 @@ Created by Patrik Rac
 Programm solving a partial differential equation of arbitrary dimensionality using the functionality of the deal.ii FE-library.
 The classes defined here can be modified in order to solve each specific Problem.
 ---------------------------------------------------------------- */
-#include "../include/problem_h.hpp"
-#include "../include/problem_hp.hpp"
+#include "problem_h.cpp"
+#include "problem_hp.cpp"
 
 using namespace dealii;
 
