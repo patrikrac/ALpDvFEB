22696be97229c2649e83d0832eb7bcedb4121a2b
Patrik Rác <patri_rac@gmx.de>
2021-12-15 16:32:01 +0100

    IMPORTANT Change to problem definition

--- mfem_general/mfem_general.cpp
+++ mfem_general/mfem_general.cpp
@@ -400,26 +400,26 @@ int main(int argc, char *argv[])
 // Exact solution, used for the Dirichlet BC.
 double bdr_func(const Vector &p)
 {
-   /*
-   double radius = sqrt(p(0) * p(0) + p(1) * p(1));
+   
+   double radius = sqrt((p(0)-0.5) * (p(0)-0.5) + p(1) * p(1));
    double phi;
    double alpha = 1.0/2.0;
 
    if (p(1) < 0)
    {
-      phi = 2 * M_PI + atan2(p(1), p(0));
+      phi = 2 * M_PI + atan2(p(1), (p(0)-0.5));
    }
    else
    {
-      phi = atan2(p(1), p(0));
+      phi = atan2(p(1), (p(0)-0.5));
    }
 
    return pow(radius, alpha) * sin(alpha * phi) * (p(2) * p(2));
-   */
+   
 
-   
+   /*
    return exp(-10 * (p(0) + p(1))) * (p(2) * p(2));
-   
+   */
 
    /*
    double k = 8.0;
@@ -430,15 +430,28 @@ double bdr_func(const Vector &p)
 // Right hand side function
 double rhs_func(const Vector &p)
 {
-   
+   /*
    return -(200 * (p(2) * p(2)) + 2) * exp(-10 * (p(0) + p(1)));
-   
+   */
 
    /*
    double k = 8.0;
    return (k * k + 4 * k - 1) * sin(k * p(0)) * cos(2 * k * p(1)) * exp(p(2));
    */
-  /*
-   return -2.0;
-   */
+  
+     
+   double radius = sqrt((p(0)-0.5) * (p(0)-0.5) + p(1) * p(1));
+   double phi;
+   double alpha = 1.0/2.0;
+
+   if (p(1) < 0)
+   {
+      phi = 2 * M_PI + atan2(p(1), (p(0)-0.5));
+   }
+   else
+   {
+      phi = atan2(p(1), (p(0)-0.5));
+   }
+
+   return -2.0 * pow(radius, alpha) * sin(alpha * phi);
 }
