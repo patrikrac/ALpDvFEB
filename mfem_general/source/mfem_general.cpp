e68c466426fde8938bde6be811b601d0402b7e27
Patrik Rác <patri_rac@gmx.de>
2021-12-16 20:47:57 +0100

    Updated programm struchture l2 ,timer etc. for parallel version

---
+++ mfem_general/source/mfem_general.cpp
@@ -0,0 +1,27 @@
+//Created by Patrik Rác
+//This is a general MFEM program designed to solve Laplace-equations using the MFEM Library.
+//The dimensionality of the programm can be changed by altering the input grid.
+
+#include "problem.cpp"
+
+int main(int argc, char *argv[])
+{
+
+  if (argc != 3)
+  {
+    std::cout << "Usage: ./mfem_general order max_dofs" << std::endl;
+    return -1;
+  }
+  // Initialize MPI
+  int num_procs, myid;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+  Problem l(num_procs, myid, std::atoi(argv[1]), std::atoi(argv[2]));
+  l.run();
+
+  //Finalize MPI
+  MPI_Finalize();
+  return 0;
+}
