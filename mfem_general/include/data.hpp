77987d07fbdc541cd0592e2234d8dc1b3a4b2613
Patrik Rác <patri_rac@gmx.de>
2021-12-17 00:08:39 +0100

    changed problem

--- mfem_general/include/data.hpp
+++ mfem_general/include/data.hpp
@@ -23,27 +23,28 @@ double bdr_func(const Vector &p)
    return pow(radius, alpha) * sin(alpha * phi) * (p(2) * p(2));
    */
 
-   
-   return exp(-10 * (p(0) + p(1))) * (p(2) * p(2));
-   
-
    /*
-   double k = 8.0;
-   return sin(k*p(0)) * cos(2*k*p(1)) * exp(p(2)); 
+   return exp(-10 * (p(0) + p(1))) * (p(2) * p(2));
    */
+
+   
+   double k = 10.0;
+   return sin(k*p(0)) * cos(2*k*p(1)) * exp(p(2)); 
+   
 }
 
 // Right hand side function
 double rhs_func(const Vector &p)
 {
    
-   return -(200 * (p(2) * p(2)) + 2) * exp(-10 * (p(0) + p(1)));
-   
-
    /*
-   double k = 8.0;
-   return (5*k*k - 1) * sin(k * p(0)) * cos(2 * k * p(1)) * exp(p(2));
+   return -(200 * (p(2) * p(2)) + 2) * exp(-10 * (p(0) + p(1)));
    */
+
+   
+   double k = 10.0;
+   return (5*k*k - 1) * sin(k * p(0)) * cos(2 * k * p(1)) * exp(p(2));
+   
   
    /*
    double radius = sqrt((p(0)-0.5) * (p(0)-0.5) + p(1) * p(1));
