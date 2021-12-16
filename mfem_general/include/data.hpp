7097e8f1d566b492f7af87e370f7f1885c4af4b4
Patrik Rác <patri_rac@gmx.de>
2021-12-16 23:57:35 +0100

    changed problem

--- mfem_general/include/data.hpp
+++ mfem_general/include/data.hpp
@@ -6,7 +6,7 @@ using namespace mfem;
 // Exact solution, used for the Dirichlet BC.
 double bdr_func(const Vector &p)
 {
-   
+   /*
    double radius = sqrt((p(0)-0.5) * (p(0)-0.5) + p(1) * p(1));
    double phi;
    double alpha = 1.0/2.0;
@@ -21,11 +21,11 @@ double bdr_func(const Vector &p)
    }
 
    return pow(radius, alpha) * sin(alpha * phi) * (p(2) * p(2));
-   
-
-   /*
-   return exp(-10 * (p(0) + p(1))) * (p(2) * p(2));
    */
+
+   
+   return exp(-10 * (p(0) + p(1))) * (p(2) * p(2));
+   
 
    /*
    double k = 8.0;
@@ -36,16 +36,16 @@ double bdr_func(const Vector &p)
 // Right hand side function
 double rhs_func(const Vector &p)
 {
-   /*
+   
    return -(200 * (p(2) * p(2)) + 2) * exp(-10 * (p(0) + p(1)));
-   */
+   
 
    /*
    double k = 8.0;
    return (5*k*k - 1) * sin(k * p(0)) * cos(2 * k * p(1)) * exp(p(2));
    */
   
-   
+   /*
    double radius = sqrt((p(0)-0.5) * (p(0)-0.5) + p(1) * p(1));
    double phi;
    double alpha = 1.0/2.0;
@@ -59,7 +59,9 @@ double rhs_func(const Vector &p)
       phi = atan2(p(1), (p(0)-0.5));
    }
    
+   
 
    return -2.0 * pow(radius, alpha) * sin(alpha * phi);
+   */
    
 }
