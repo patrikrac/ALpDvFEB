bed52fa2b83747b565252322f204cf9eae62c1b8
Patrik Rac <patrik_rac@gmx.de>
2021-12-27 15:30:33 +0100

    Experimental tanh problem setup

--- mfem_general/source/problem.cpp
+++ mfem_general/source/problem.cpp
@@ -5,7 +5,7 @@
 // Exact solution, used for the dirichlet BC.
 double bdr_func(const Vector &p)
 {
-   
+   /*
    double radius = sqrt((p(0)-0.5) * (p(0)-0.5) + p(1) * p(1));
    double phi;
    double alpha = 1.0/2.0;
@@ -20,7 +20,7 @@ double bdr_func(const Vector &p)
    }
 
    return pow(radius, alpha) * sin(alpha * phi) * (p(2) * p(2));
-   
+   */
 
    /*
    return exp(-10 * (p(0) + p(1))) * (p(2) * p(2));
@@ -30,12 +30,16 @@ double bdr_func(const Vector &p)
    double k = 10.0;
    return sin(k*p(0)) * cos(2*k*p(1)) * exp(p(2)); 
    */
+
+   double radius = sqrt(p(0) * p(0) + p(1) * p(1));
+   double k = 100;
+   return tanh(k * (radius - 1.0 / 2.0));
 }
 
 // Right hand side function
 double rhs_func(const Vector &p)
 {
-   
+
    /*
    return -(200 * (p(2) * p(2)) + 2) * exp(-10 * (p(0) + p(1)));
    */
@@ -44,8 +48,8 @@ double rhs_func(const Vector &p)
    double k = 10.0;
    return (5*k*k - 1) * sin(k * p(0)) * cos(2 * k * p(1)) * exp(p(2));
    */
-  
-   
+
+   /*
    double radius = sqrt((p(0)-0.5) * (p(0)-0.5) + p(1) * p(1));
    double phi;
    double alpha = 1.0/2.0;
@@ -62,6 +66,9 @@ double rhs_func(const Vector &p)
    
 
    return -2.0 * pow(radius, alpha) * sin(alpha * phi);
-   
-   
+   */
+  double radius = sqrt(p(0) * p(0) + p(1) * p(1));
+  double k = 100;
+
+   return (-1.0/radius + 2.0*k*tanh(k*(radius - 1.0/2.0)))*k*(1-(tanh(k*(radius - 1.0/2.0))*tanh(k*(radius - 1.0/2.0))));
 }
