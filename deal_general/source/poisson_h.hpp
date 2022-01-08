3a04dcabb7053cb7c40cae376faecee65e6f7bed
Patrik Rac <patrik_rac@gmx.de>
2022-01-08 12:32:33 +0100

    boundary value fix

--- deal_general/source/poisson_h.hpp
+++ deal_general/source/poisson_h.hpp
@@ -222,7 +222,7 @@ namespace AspDEQuFEL
         constraints.clear();
         DoFTools::make_hanging_node_constraints(dof_handler, constraints);
 
-        VectorTools::interpolate_boundary_values(dof_handler, BoundaryValues<dim>(), constraints);
+        VectorTools::interpolate_boundary_values(dof_handler, 0, BoundaryValues<dim>(), constraints);
 
         constraints.close();
 
