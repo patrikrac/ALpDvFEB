458593033c500dd77cc37e0b4f2293fcff77ac49
Patrik Rac <patrik_rac@gmx.de>
2022-01-15 14:40:34 +0100

    test

--- ngsolve_general/mpi_test.py
+++ ngsolve_general/mpi_test.py
@@ -6,18 +6,19 @@ import netgen.meshing
 from ngsolve import *
 
 # initialize MPI
-comm = MPI.COMM_WORLD
-comm = MPI_Init()
+comm = mpi_world
 rank = comm.rank
 np = comm.size
 
-do_vtk = False
 
 print("Hello from rank "+str(rank)+" of "+str(np))
 
-mesh = Mesh(unit_cube.GenerateMesh(maxh=0.5))
+if comm.rank == 0:
+    ngmesh = unit_cube.GenerateMesh(maxh=0.1).Distribute(comm)
+else:
+    ngmesh = netgen.meshing.Mesh.Receive(comm)
 
+mesh = Mesh(ngmesh)
 
-    
 print('rank', str(comm.rank)+"'s part of the mesh has ", mesh.ne, 'elements, ', \
       mesh.nface, 'faces, ', mesh.nedge, 'edges and ', mesh.nv, ' vertices')
