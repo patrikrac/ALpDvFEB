0b877b24917d199b0220dc814f168298cccefc29
Patrik Rac <patrik_rac@gmx.de>
2022-01-15 14:34:58 +0100

    changed test

--- ngsolve_general/mpi_test.py
+++ ngsolve_general/mpi_test.py
@@ -14,10 +14,10 @@ np = comm.size
 do_vtk = False
 
 print("Hello from rank "+str(rank)+" of "+str(np))
-if comm.rank == 0:
-    ngmesh = unit_cube.GenerateMesh(maxh=0.1).Distribute(comm)
-else:
-    ngmesh = netgen.meshing.Mesh.Receive(comm)
+
+mesh = Mesh(unit_cube.GenerateMesh(maxh=0.1))
+
+
     
 print('rank', str(comm.rank)+"'s part of the mesh has ", mesh.ne, 'elements, ', \
       mesh.nface, 'faces, ', mesh.nedge, 'edges and ', mesh.nv, ' vertices')
