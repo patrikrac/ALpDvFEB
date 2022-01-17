346fbcefbe92a281f5d6317da34aaff0facf75fd
Patrik Rac <patrik_rac@gmx.de>
2022-01-17 15:08:03 +0100

    V1.0.11 Additional Fix

--- ngsolve_general/ngsolve-general.py
+++ ngsolve_general/ngsolve-general.py
@@ -147,8 +147,9 @@ class Poisson:
         f += self.rhs*v*dx
         
         #Define the solver to be used to solve the problem
-        c = MultiGridPreconditioner(a, inverse = "sparsecholesky")
-
+        #c = MultiGridPreconditioner(a, inverse = "sparsecholesky")
+        c = Preconditioner(a, type="bddc")
+        
         return (a,f,c)
     
     
@@ -344,7 +345,7 @@ class Poisson:
 
         f.write("\t\t\tcycle & $n_{cells} $ & $n_{dof}$ & $\\left\\|u_h - I_hu\\right\\| _{L_2}$ & $\\left\\|u_h - I_hu\\right\\| _{L_\\infty}$ & $t_{solve}$ & $t_{refine}$ & $t_{assembly}$\\\\ \\hline\n")
         for m in self.table_list:
-            f.write("\t\t\t{} & {} & {} & {:.3e} & {:.3e} & {:.3e} & {:.3e} & {:.3e}\\\ \hline\n".format(m.cycle, m.cells, m.dofs, m.l2_error, m.max_error, m.solution_time, m.refinement_time, m.assembly_time))
+            f.write("\t\t\t{} & {} & {} & {:.3e} & {:.3e} & {:.3e} & {:.3e} & {:.3e}\\\\ \hline\n".format(m.cycle, m.cells, m.dofs, m.l2_error, m.max_error, m.solution_time, m.refinement_time, m.assembly_time))
             plot_l2.write("{} {}\n".format(m.dofs, m.l2_error))
             plot_max.write("{} {}\n".format(m.dofs, m.max_error))
             plot_p1.write("{} {}\n".format(m.dofs, m.error_p1))
