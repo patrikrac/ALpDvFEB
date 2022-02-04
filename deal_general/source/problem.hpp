249f5daba5a13bc9ee1582d950e686b14ae96a34
Patrik Rac <patrik_rac@gmx.de>
2022-02-04 15:40:58 +0100

    Switched to oscillating problem

--- deal_general/source/problem.hpp
+++ deal_general/source/problem.hpp
@@ -41,7 +41,7 @@ template <int dim>
 double BoundaryValues<dim>::value(const Point<dim> &p, const unsigned int /*component*/) const
 {
     //Formulate the boundary function
-    
+    /*
     //Problem with singularity 
     double alpha = 1.0/2.0;
     double radius = sqrt((p(0)-0.5)*(p(0)-0.5) + p(1)*p(1));
@@ -56,12 +56,12 @@ double BoundaryValues<dim>::value(const Point<dim> &p, const unsigned int /*comp
     }
 
     return pow(radius, alpha) * sin(alpha * phi) * (p(2)*p(2));
+    */
     
-    /*
    //Problem using the highly oszilating function
    double k = 10.0;
    return sin(k*p(0)) * cos(2*k*p(1)) * exp(p(2));
-   */
+   
 
     
     //Problem using the exponential function
@@ -75,7 +75,7 @@ template <int dim>
 double RHS_function<dim>::value(const Point<dim> &p, const unsigned int /*component*/) const
 {
     //Formulate right hand side function
-    
+    /*
     double alpha = 1.0/2.0;
     double radius = sqrt((p(0)-0.5)*(p(0)-0.5) + p(1)*p(1));
     double phi;
@@ -89,12 +89,12 @@ double RHS_function<dim>::value(const Point<dim> &p, const unsigned int /*compon
     }
 
     return -2.0*pow(radius, alpha) * sin(alpha * phi);
-    
+    */
 
-    /*
+    
     double k = 10.0;
     return (5*k*k- 1) * sin(k * p(0)) * cos(2 * k * p(1)) * exp(p(2));
-    */
+    
 
    
     //return -(200 * (p(2) * p(2)) + 2) * exp(-10 * (p(0) + p(1)));
@@ -106,7 +106,7 @@ template <int dim>
 double Solution<dim>::value(const Point<dim> &p, const unsigned int /*component*/) const
 {
     //Formulate the boundary function
-    
+    /*
     //Problem with singularity
     double alpha = 1.0/2.0;
     double radius = sqrt((p(0)-0.5)*(p(0)-0.5) + p(1)*p(1));
@@ -121,12 +121,12 @@ double Solution<dim>::value(const Point<dim> &p, const unsigned int /*component*
     }
 
     return pow(radius, alpha) * sin(alpha * phi) * (p(2)*p(2));
+    */
     
-    /*
    //Problem using the highly oszilating function
    double k = 10.0;
    return sin(k*p(0)) * cos(2*k*p(1)) * exp(p(2));
-   */
+   
 
     
     //Problem using the exponential function
